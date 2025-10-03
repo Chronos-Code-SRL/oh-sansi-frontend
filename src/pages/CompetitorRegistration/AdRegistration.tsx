22d1b8454fde0c1c71a2abfe7b6234a561faed41
Edeny Fernandez <edenysfq003@gmail.com>
2025-10-03 16:04:09 -0400

    Corrections without modal

--- src/pages/CompetitorRegistration/AdRegistration.tsx
+++ src/pages/CompetitorRegistration/AdRegistration.tsx
@@ -3,11 +3,9 @@ import PageMeta from "../../components/common/PageMeta";
 import { useState } from "react";
 import ComponentCard from "../../components/common/ComponentCard";
 import Button from "../../components/ui/button/Button";
-import { Modal } from "../../components/ui/modal";
-import { useModal } from "../../hooks/useModal";
-import Alert from "../../components/ui/alert/Alert";
-import { DownloadIcon } from "../../icons";
+import { CheckCircleIcon, DownloadIcon, ErrorIcon, FileIcon, InfoIcon } from "../../icons";
 import TitleBreadCrumb from "../../components/common/TitleBreadCrumb";
+import Badge from "../../components/ui/badge/Badge";
 //import Dropzone from "react-dropzone";
 
 type UploadedFile = {
@@ -15,7 +13,6 @@ type UploadedFile = {
   size: string;
 };
 export default function AdRegistration() {
-  const { isOpen, openModal, closeModal } = useModal();
   const [files, setFiles] = useState<UploadedFile[]>([]);
 
   const handleFilesAdded = (acceptedFiles: File[]) => {
@@ -46,14 +43,27 @@ export default function AdRegistration() {
                   <div className="mt-6 text-left">
                     <div className="space-y-2">
                       {files.map((f, idx) => (
-                        <div
-                          key={idx}
-                          className="flex justify-between items-center p-3 border rounded-xl"
-                        >
-                          <div>
-                            <p className="font-medium">{f.name}</p>
-                            <p className="text-sm text-gray-500">{f.size}</p>
+                         <div
+                        key={idx}
+                        className="flex flex-col sm:flex-row sm:items-start sm:justify-between p-4 border rounded-xl gap-3"
+  >
+                         <div className="flex items-start space-x-3">
+                            <FileIcon className="w-6 h-6 text-gray-600 mt-1" />
+                            <div>
+                              <p className="font-medium">{f.name}</p>
+                              <p className="text-sm text-gray-500">{f.size}</p>
+                              <div className="mt-1 space-x-2">
+                                <Badge color="info" startIcon={<InfoIcon className="size-5" />} >72 registros totales</Badge>
+                                <Badge color="success" startIcon={<CheckCircleIcon className="size-5" />}>55 exitosos</Badge>
+                                <Badge color="error" startIcon={<ErrorIcon className="size-5" />}>17 errores</Badge>
+                              </div>
+                            </div>
                           </div>
+                            <div className="sm:ml-auto ">      
+                              <Button size="sm" variant= "outline" startIcon={<DownloadIcon className="size-5" />}> 
+                                Descargar CSV de errores
+                              </Button>
+                            </div>
                           <span className="text-green-600 font-semibold"></span>
                         </div>
                       ))}
@@ -61,67 +71,9 @@ export default function AdRegistration() {
                   </div>                  
                 )
                 }
-                <div className="flex items-center gap-3 px-2 mt-6 justify-end">
-                  <Button size="sm" onClick={(openModal)}>
-                    Inscribir
-                  </Button>
-                </div>
+                
               </ComponentCard>
-              <Modal isOpen={isOpen} onClose={closeModal} className="max-w-[700px] m-4">
-                <div className="no-scrollbar relative w-full max-w-[700px] overflow-y-auto rounded-3xl bg-white p-4 dark:bg-gray-900 lg:p-11">
-                  {/* Header */}
-                  <div className="px-2 pr-14 text-left mb-8">
-                    <h4 className="mb-2 text-2xl font-semibold text-gray-800 dark:text-white/90">
-                      Resultado del Procesamiento
-                    </h4>
-                    <p className="mb-4 text-sm text-gray-500 dark:text-gray-400 lg:mb-4">
-                      Archivo: Copia de VelocidadDeConsultas.csv
-                    </p>
-                  </div>
-
-                  {/* Cifras principales */}
-                  <div className="grid grid-cols-3 gap-4 mb-6">
-                    <div className="rounded-xl border p-4 text-center bg-gray-50 dark:bg-gray-800">
-                      <p className="text-sm text-gray-500">Total Registros</p>
-                      <p className="text-2xl font-bold">148</p>
-                    </div>
-                    <div className="rounded-xl border p-4 text-center bg-green-50 border-green-200 dark:bg-green-900/20">
-                      <p className="text-sm text-gray-500">Exitosos</p>
-                      <p className="text-2xl font-bold text-green-600">140</p>
-                    </div>
-                    <div className="rounded-xl border p-4 text-center bg-red-50 border-red-200 dark:bg-red-900/20">
-                      <p className="text-sm text-gray-500">Con Errores</p>
-                      <p className="text-2xl font-bold text-red-600">8</p>
-                    </div>
-                  </div>
-
-                  {/* Advertencia */}
-                  <div className="mb-6 text-left">
-                    <Alert
-                      variant="warning"
-                      title="Procesado con Advertencias"
-                      message="Se procesaron 140 registros correctamente, pero 8 registros tienen errores que deben ser corregidos."
-                      showLink={false}
-                    />
-                  </div>
-
-                  {/* Detalle de errores */}
-                  <div className="flex items-center justify-between mb-3">
-                    <h5 className="text-lg font-semibold">Detalle de Errores</h5>
-                    <Button size="sm" variant= "outline" startIcon={<DownloadIcon className="size-5" />} onClick={closeModal} > 
-                      Descargar CSV de errores
-                    </Button>
-                  </div>
-                  
-                  {/* Botón inferior */}
-                  <div className="flex items-center gap-3 px-2 mt-6 lg:justify-end">
-                    <Button onClick={closeModal}>
-                      Cerrar
-                    </Button>
-                     
-                  </div>
-                </div>
-              </Modal>
+             
 
         </div>
       </div>
