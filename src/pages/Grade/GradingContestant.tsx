984a7ab711cfb83c1a3d28e35e6a765b2727177e
Jose Erick Jesus Gutierrez <josejesusgutierrez11@gmail.com>
2025-10-28 00:38:32 -0400

    Dynamic tables and titles

--- src/pages/Grade/GradingContestant.tsx
+++ src/pages/Grade/GradingContestant.tsx
@@ -1,17 +1,27 @@
+import { useParams } from "react-router";
 import ComponentCard from "../../components/common/ComponentCard";
 import PageMeta from "../../components/common/PageMeta";
 import StudentTable from "../../components/Grade/StudentTable";
 
 
 export default function MarksStudents() {
+    function capitalizeFirst(str: string) {
+        const s = str.trim();
+        if (!s) return s;
+        return s.charAt(0).toUpperCase() + s.slice(1);
+    }
+
+    const { areaName, areaId } = useParams<{ areaName?: string; areaId?: string }>();
+    const baseTitle = areaName ? decodeURIComponent(areaName) : "Calificaciones";
+    const title = capitalizeFirst(baseTitle);
     return (
         <>
             <PageMeta
-                title={"Calificaciones de Estudiantes"}
+                title={title}
                 description={"En esta sección puedes ver y gestionar las calificaciones de los estudiantes."}
             />
-            <ComponentCard title="Materia Fase tal">
-                <StudentTable />
+            <ComponentCard key={areaId} title={title}>
+                <StudentTable key={areaId} idArea={Number(areaId)} />
             </ComponentCard>
         </>
     )
