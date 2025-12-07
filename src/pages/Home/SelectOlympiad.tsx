ebc4c8794ad3ec737c49a1507ed0a492ca233326
Jose Erick Jesus Gutierrez <josejesusgutierrez11@gmail.com>
2025-12-07 18:56:51 -0400

    Add Administrator view

--- src/pages/Home/SelectOlympiad.tsx
+++ src/pages/Home/SelectOlympiad.tsx
@@ -1,20 +1,10 @@
 import LadingPage from "../../components/home/LadingPageOlympiad"
-import { Navigate } from "react-router";
-import {getRoleName, getUser } from "../../api/services/authService";
 
 export const SelectOlympiad = () => {
 
-  const user = getUser();
-  const roleNames = getRoleName(user);
-  const isAdmin = roleNames[0] === "Admin";
-  // Si es Admin (por nombre de rol), redirige al dashboard con sidebar y evita renderizar esta página
-  if (isAdmin) {
-    return <Navigate to="/" replace />;
-  }
-
   return (
     <>
-    <LadingPage />
+      <LadingPage />
     </>
   )
 }
